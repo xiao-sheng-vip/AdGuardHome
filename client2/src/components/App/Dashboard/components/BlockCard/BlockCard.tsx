cf431f1a1b4da6809d9033d59b9bf20e63ca4bec
Vlad <v.abdulmyanov@adguard.com>
2021-01-18 14:17:46 +0300

    wip

--- client2/src/components/App/Dashboard/components/BlockCard/BlockCard.tsx
+++ client2/src/components/App/Dashboard/components/BlockCard/BlockCard.tsx
@@ -4,22 +4,30 @@ import { AreaChart, Area, ResponsiveContainer } from 'recharts';
 import s from './BlockCard.module.pcss';
 
 interface BlockCardProps {
-    overal: number;
-    data: number[];
-    color: string;
+    overal: number | string;
+    data?: number[];
+    text?: string;
+    color?: string;
     title: string;
 }
 
-const BlockCard: FC<BlockCardProps> = ({ overal, data, color, title }) => {
+const BlockCard: FC<BlockCardProps> = ({ overal, data, color, title, text }) => {
     return (
         <div className={s.container}>
             <div className={s.title}>{title}</div>
             <div className={s.overal}>{overal}</div>
-            <ResponsiveContainer width="100%" height={25}>
-                <AreaChart data={data.map((n) => ({ name: 'data', value: n }))}>
-                    <Area dataKey="value" stroke={color} fill={color} dot={false} />
-                </AreaChart>
-            </ResponsiveContainer>
+            {data && (
+                <ResponsiveContainer width="100%" height={25}>
+                    <AreaChart data={data.map((n) => ({ name: 'data', value: n }))}>
+                        <Area dataKey="value" stroke={color} fill={color} dot={false} />
+                    </AreaChart>
+                </ResponsiveContainer>
+            )}
+            {text && (
+                <div className={s.title}>
+                    {text}
+                </div>
+            )}
         </div>
     );
 };
